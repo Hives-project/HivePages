ffe1c73ecbd3a4c114ff24ea152c6eabfd3baf70
Yoeri <yoerisetz02@gmail.com>
2023-01-12 10:59:34 +0100

    add accesstoken to k6 tests

--- k6-tests.js
+++ k6-tests.js
@@ -2,21 +2,24 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  duration: '30s',
-  vus: 50,
+  stages: [
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 20 },
+  ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+    http_req_duration: ['avg<100', 'p(95)<200'], // 95 percent of response times must be below 500ms
   },
 };
 
-export default function () {
-  const res = http.post('https://lemur-18.cloud-iam.com/auth/realms/anonymizer-iam/protocol/openid-connect/token', 
-    
-  )
+export function setup() {
+  let params = {client_id: "service-client",  client_secret: "8oQmmPpyQqWMAnMTMIuEyQyAZ7m2GKCI", grant_type: "password", username: "yoeri@mail.com", password: "1qazxsw2"}
+  const res = http.post('https://lemur-18.cloud-iam.com/auth/realms/anonymizer-iam/protocol/openid-connect/token', params)
+  return res.json("access_token")
 }
 
-export default function () {
-  const res = http.get('http://34.79.251.22:/pages/12');
-  sleep(1);
+export default function getpage(accesstoken) {
+  let params = {headers: { 'Authorization': "Bearer "+accesstoken }}
+  const res = http.get('http://34.79.251.22:/pages/12', params);
+  sleep(500);
 }
